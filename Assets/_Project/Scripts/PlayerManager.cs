8f1576f0dfb43020098aa1f9fa4a13ae3390276c
Moritz Reinert <mr148@hdm-stuttgart.de>
2018-01-27 11:51:37 +0100

    Scrolling Background & proper setup

--- Assets/_Project/Scripts/PlayerManager.cs
+++ Assets/_Project/Scripts/PlayerManager.cs
@@ -8,17 +8,21 @@ public class PlayerManager : MonoBehaviour {
     [Range(0,10)]
     public float MovementSpeed;
 
+    private Transform _parent;
+
     public MeshRenderer Ren;
 
 	// Use this for initialization
 	void Start () {
-		
+
+        _parent = transform.parent;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        transform.DOMoveX(transform.position.x + MovementSpeed * Time.deltaTime,0);
+        _parent.DOMoveX(_parent.position.x + MovementSpeed * Time.deltaTime,0);
         Ren.material.mainTextureOffset = new Vector2(Ren.material.mainTextureOffset.x + Time.deltaTime/MovementSpeed,0);
 
 	}
