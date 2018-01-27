3df77ea658f3c096679a6856e6479278ff22dd59
Moritz Reinert <mr148@hdm-stuttgart.de>
2018-01-27 01:30:19 +0100

    Scrolling Background Quad

--- Assets/_Project/Scripts/BodyPartManager.cs
+++ Assets/_Project/Scripts/BodyPartManager.cs
@@ -26,6 +26,11 @@ public class BodyPartManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            Animator.Play(Animator.StringToHash("Idle"));
+        }
 		
 	}
 
---
+++ Assets/_Project/Scripts/PlayerManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PlayerManager : MonoBehaviour {
+
+    [Range(0,10)]
+    public float MovementSpeed;
+
+    public MeshRenderer Ren;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.DOMoveX(transform.position.x + MovementSpeed * Time.deltaTime,0);
+        Ren.material.mainTextureOffset = new Vector2(Ren.material.mainTextureOffset.x + Time.deltaTime/MovementSpeed,0);
+
+	}
+}
