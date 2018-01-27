6caaa2a15518f3692be710499090d912821437de
Simon <ss387@hdm-stuttgart.de>
2018-01-27 23:37:56 +0100

    merginskaski

--- Assets/_Project/Scripts/LevelManager.cs
+++ Assets/_Project/Scripts/LevelManager.cs
@@ -104,9 +104,11 @@ public class LevelManager : MonoBehaviour {
         seq.OnComplete(() =>
         {
 
+
             _playerMan.transform.rotation = Quaternion.Euler(new Vector3(0,0,0));
             _playerMan.GetComponent<SpriteRenderer>().DOFade(1, 0f);
             _playerMan.transform.DOScale(Vector3.one, 0.2f).OnComplete(() => _playerMan.StopRunning = false);
+
 
         });
 
